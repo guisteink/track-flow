593e932f8bda699f063ba616954e4394ab6569a5
guilherme stein <guilherme.steink@gmail.com>
2025-03-10 20:45:50 -0300

    add java21-maven project

---
+++ src/main/java/com/example/track_flow/TrackFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.track_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/track_flow/TrackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.track_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
