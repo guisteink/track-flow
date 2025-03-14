3e772907dff06cf3cf4f41a2fada0f469d922c03
guilherme stein <guilherme.steink@gmail.com>
2025-03-14 16:39:10 -0300

    feat: enable caching and asynchronous processing in TrackFlowApplication

--- src/main/java/com/example/track_flow/TrackFlowApplication.java
+++ src/main/java/com/example/track_flow/TrackFlowApplication.java
@@ -1,22 +1,21 @@
 package com.example.track_flow;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EntityScan("com.example.track_flow.model")
 @EnableJpaRepositories("com.example.track_flow.repository")
 @ComponentScan("com.example.track_flow")
+@EnableCaching
+@EnableAsync
 public class TrackFlowApplication {
-    private static final Logger logger = LoggerFactory.getLogger(TrackFlowApplication.class);
-
     public static void main(String[] args) {
-        logger.info("Iniciando Track Flow Application!!!");
         SpringApplication.run(TrackFlowApplication.class, args);
     }
 }
